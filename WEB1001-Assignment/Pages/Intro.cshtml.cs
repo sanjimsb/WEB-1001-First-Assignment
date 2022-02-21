ae58864f83a8be1c46442daf0f9702c704a591bd
Bipin MSB <bpmsb45@gmail.com>
2022-02-20 21:45:43 -0500

    Added razor page named intro and update site.css

---
+++ WEB1001-Assignment/Pages/Intro.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WEB1001_Assignment.Pages
+{
+    public class IntroModel : PageModel
+    {
+        private readonly ILogger<IntroModel> _logger;
+
+        public IntroModel(ILogger<IntroModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
